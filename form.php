eb037c6d0642f8bc9f3256c488dd32557fd80af5
Zac <a19860101@gmail.com>
2024-11-12 19:44:24 +0800

    20241112

--- form.php
+++ form.php
@@ -15,6 +15,39 @@
             <label for="">電話</label>
             <input type="text" name="phone">
         </div>
+        <div>
+            <label for="">學歷</label>
+            <select name="edu" id="">
+                <option value="國小">國小</option>
+                <option value="國中">國中</option>
+                <option value="高中職">高中職</option>
+                <option value="大專院校">大專院校</option>
+            </select>
+        </div>
+        <div>
+            <label for="">性別</label>
+            <input type="radio" name="gender" value="男">
+            <label for="">男</label>
+            <input type="radio" name="gender" value="女">
+            <label for="">女</label>
+            <input type="radio" name="gender" value="不透露">
+            <label for="">不透露</label>
+        </div>
+        <div>
+            <label for="">專長</label>
+            <input type="checkbox" name="skill[]" value="數學">
+            <label for="">數學</label>
+            <input type="checkbox" name="skill[]" value="物理">
+            <label for="">物理</label>
+            <input type="checkbox" name="skill[]" value="化學">
+            <label for="">化學</label>
+            <input type="checkbox" name="skill[]" value="英文">
+            <label for="">英文</label>
+        </div>
+        <div>
+            <label for="">備註</label>
+            <textarea name="comment" id=""></textarea>
+        </div>
         <input type="submit">
     </form>
     <!-- 
--- response.php
+++ response.php
@@ -10,7 +10,56 @@
     // echo $_REQUEST['name'];
     // echo '<br>';
     // echo $_REQUEST['phone'];
+    
+    // echo $_REQUEST['name'];
+    // echo '<br>';
+    // echo $_REQUEST['phone'];
+    // echo '<br>';
+    // echo $_REQUEST['edu'];
+    // echo '<br>';
+    // echo $_REQUEST['gender'];
+    // echo '<br>';
+    
     extract($_REQUEST);
-    echo $name;
-    echo '<br>';
-    echo $phone;
+    // echo $name;
+    // echo '<br>';
+    // echo $phone;
+    // echo '<br>';
+    // echo $edu;
+    // echo '<br>';
+    // echo $gender;
+    // echo '<br>';
+    // print_r($skill);
+    // echo implode(',',$skill);
+    // echo '<br>';
+    // echo $comment;
+    function check($input){
+        // 去除前後空白字元
+        $input = trim($input); 
+        //去除反斜線\
+        $input = stripslashes($input); 
+        //將特殊字元實體化
+        $input = htmlspecialchars($input); 
+        return $input;
+    }
+
+    if(check($name) == ''){
+        echo '請輸入姓名';
+    }else{
+        $name = check($name);
+        echo "<div>姓名:{$name}</div>";
+    }
+
+    if(empty(check($phone))){
+        echo '請輸入電話';
+    }else{
+        $phone = check($phone);
+        echo "<div>電話:{$phone}</div>";
+    }
+    if(check($comment) == null){
+        echo '請輸入備註';
+    }else{
+        $comment = check($comment);
+        echo "<div>備註:{$comment}</div>";
+    }
+
