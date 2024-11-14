84c367119e0a8450a1ac711be726b9efd662b4b2
Zac <a19860101@gmail.com>
2024-11-14 21:21:07 +0800

    20241114

---
+++ students/create.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>新增資料</h1>
+    <form action="" method="post">
+        <div>
+            <label for="">姓名</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="">性別</label>
+            <input type="radio" name="gender" value="女">
+            <label for="">女</label>
+            <input type="radio" name="gender" value="男">
+            <label for="">男</label>
+            <input type="radio" name="gender" value="不透露">
+            <label for="">不透露</label>
+        </div>
+        <div>
+            <label for="">電話</label>
+            <input type="text" name="phone">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="">地址</label>
+            <input type="text" name="address">
+        </div>
+        <input type="submit" value="新增資料">
+        <input type="button" value="取消" onclick="history.back()">
+    </form>
+</body>
+</html>
--- students/index.php
+++ students/index.php
@@ -3,8 +3,8 @@
     $sql = 'SELECT * FROM students';
     // $result = $pdo->query($sql);
     // $students = $result->fetchAll();
-    $students = $pdo->query($sql)->fetchAll();
-    print_r($students);
+    $students = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    // print_r($students);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,12 +24,31 @@
 </head>
 <body>
     <h1>學員通訊錄</h1>
-    <a href="#">新增學員資料</a>
+    <a href="create.php">新增學員資料</a>
     <table>
         <tr>
             <th>id</th>
             <th>姓名</th>
+            <th>性別</th>
         </tr>
+        <?php foreach($students as $student){ ?>
+        <tr>
+            <td><?php echo $student['id'];?></td>
+            <td><?php echo $student['name'];?></td>
+            <td><?php echo $student['gender'];?></td>
+        </tr>
+        <?php } ?>
+
+        <?php 
+            // foreach($students as $student){
+            //     echo '<tr>';
+            //     echo '<td>'.$student['id'].'</td>';
+            //     echo '<td>'.$student['name'].'</td>';
+            //     echo '<td>'.$student['gender'].'</td>';
+            //     echo '</tr>';
+
+            // }
+        ?>
     </table>
 </body>
 </html>
