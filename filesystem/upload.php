d615f3c1bdf3baf16b80c17d902a6f74ee394447
Zac <a19860101@gmail.com>
2024-11-19 20:55:57 +0800

    20241119

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -1,2 +1,16 @@
 <?php
-    print_r($_FILES);
+    extract($_FILES['data']);
+
+    $target = 'images/'.$name;
+
+    if($error == 0){
+        if(move_uploaded_file($tmp_name,$target)){
+            echo '<script>alert("檔案已上傳")</script>';
+            header('refresh:0;url=index.php');
+        }
+    }else{
+        echo '<script>alert("發生錯誤，請洽網站管理員")</script>';
+        header('refresh:0;url=index.php');
+
+
+    }
