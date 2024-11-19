b3331e0ec1dfec6b5ada3ac772142e9f0953e874
Zac <a19860101@gmail.com>
2024-11-19 20:37:12 +0800

    20241119

---
+++ filesystem/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <div>
+            <label for="">上傳檔案</label>
+            <input type="file" name="data">
+        </div>
+        <input type="submit" value="上傳">
+    </form>
+</body>
+</html>
