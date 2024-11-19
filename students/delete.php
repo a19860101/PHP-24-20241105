e873b5ac21d1f29dbaaf4e9c23a558932c1fa410
Zac <a19860101@gmail.com>
2024-11-19 20:23:16 +0800

    20241119

---
+++ students/delete.php
@@ -0,0 +1,8 @@
+<?php 
+    include('db.php');
+    $sql = 'DELETE FROM students WHERE id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_REQUEST['id']]);
+
+    echo '<script>alert("學員資料已刪除");</script>';
+    header('refresh:0;url=index.php');
