273a062a4b2875a40f9035e2511a3e7279fd681a
Zac <a19860101@gmail.com>
2024-11-28 19:24:17 +0800

    test

--- member/function.php
+++ member/function.php
@@ -3,10 +3,39 @@
     //會員註冊
     function store($request){
         extract($request);
-        
         $sql = 'INSERT INTO users(username,password,created_at)VALUES(?,?,?)';
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $stmt = pdo()->prepare($sql);
-        $stmt->execute([$username, $password, now()]);
+        
+        if(checkUser($username) != 0){
+            return checkUser($username);
+        }
+        try{
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $stmt = pdo()->prepare($sql);
+            $stmt->execute([$username, $password, now()]);
+            return [
+                'errorCode' => 0,
+                'status' => '註冊成功'
+            ];
+        }catch(PDOException $e){
+            echo $e->getMessage();
+            return [
+                'errorCode' => 1,
+                'status' => '註冊失敗'
+            ];
+        }
     
+    }
+    function checkUser($username){
+        $sql = 'SELECT * FROM users WHERE username = ?';
+        $stmt = pdo()->prepare($sql);
+        $stmt->execute([$username]);
+        // return $stmt->rowCount();
+        if($stmt->rowCount() > 0){
+            return [
+                'errorCode' => 2,
+                'status' => '帳號重複，請重新註冊'
+            ];
+        }else{
+            return 0;
+        }
     }
--- member/store.php
+++ member/store.php
@@ -2,7 +2,18 @@
 
     include('function.php');
 
-    store($_REQUEST);
+    $result = store($_REQUEST);
+    if($result['errorCode'] == 0){
+        echo '<script>alert("申請成功，請重新登入")</script>';
+        header('refresh:0;url=index.php');
+    }
+    if($result['errorCode'] == 1){
+        echo '<script>alert("申請失敗，請重新登入")</script>';
+        header('refresh:0;url=index.php');
+    }
+    if($result['errorCode'] == 2){
+        echo '<script>alert("申請失敗，請重新申請")</script>';
+        header('refresh:0;url=index.php');
+    }
 
-    echo '<script>alert("申請成功，請重新登入")</script>';
-    header('refresh:0;url=index.php');
+    
