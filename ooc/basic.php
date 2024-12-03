e64cd26b9b7d35c5c9e0ab6c32d24882eebc3719
Zac <a19860101@gmail.com>
2024-12-03 19:54:07 +0800

    20241203

--- ooc/basic.php
+++ ooc/basic.php
@@ -12,6 +12,8 @@
         public $eye_color;
         public $hair_color;
         public $name = 'anounmous';
+        // private $hp = 100;
+        protected $hp = 100;
 
         // 方法
         function walk(){
@@ -24,6 +26,9 @@
         function jump(){
             return $this->name.' jump';
         }
+        function showHP(){
+            return $this->hp;
+        }
     }
 
     // 建立實體
@@ -34,12 +39,33 @@
     $human1->hair_color = 'Black';
     $human1->name = 'Mary';
     // echo $human1->jump();
-    echo $human1->walk();
-    print_r($human1);
+    // echo $human1->walk();
+    // print_r($human1);
+    // echo $human1->hp;
+    // echo $human1->showHP();
 
     $human2 = new Human;
     $human2->name = 'Micheal';
-    print_r($human2);
-    echo $human2->jump();
+    // print_r($human2);
+    // echo $human2->jump();
+    echo $human2->run();
 
 
+    class NPC extends Human{
+        public $name = 'npc';
+        function showHP(){
+            return $this->hp;
+        }
+    }
+
+    $n1 = new NPC;
+    echo $n1->showHP();
+    // print_r($n1);
+    // echo $n1->jump();
+
+
+    /* 
+        public 可使用在所有地方
+        private 只能使用在自己的類別內
+        protected 可使用在類別內，包含繼承
+    */
