a936da0e9688c0e4e948c306662295fc70868b7f
Zac <a19860101@gmail.com>
2024-12-05 19:29:15 +0800

    20241205

--- web/public/create.php
+++ web/public/create.php
@@ -1,3 +1,17 @@
 <?php include('template/header.php'); ?>
-<h1>建立文章</h1>
+<div class="container mx-auto">
+    <div class="w-1/2 p-8">
+        <form action="">
+            <div class="mb-3">
+                <label for="">文章標題</label>
+                <input type="text" name="title" class="border border-zinc-700 rounded-lg">
+            </div>
+            <div class="mb-3">
+                <label for="">文章內容</label>
+                <textarea name="body" id="" class="border border-zinc-700 rounded-lg"></textarea>
+            </div>
+            <input type="submit" value="建立文章" class="py-2 px-8 bg-sky-400 rounded-lg">
+        </form>
+    </div>
+</div>
 <?php include('template/footer.php'); ?>
