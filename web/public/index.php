e90c5d69c798d01f615c95b6438f56d971fb0734
Zac <a19860101@gmail.com>
2024-12-05 19:12:08 +0800

    20241205

--- web/public/index.php
+++ web/public/index.php
@@ -3,18 +3,10 @@
     use Zac\Web\Config\DB;
 
     // print_r(DB::db());
-    echo Zac\Web\Config\DB::now();
-    echo DB::now();
+    // echo Zac\Web\Config\DB::now();
+    // echo DB::now();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include('template/header.php'); ?>
+<h1>test</h1>
+<?php include('template/footer.php'); ?>
