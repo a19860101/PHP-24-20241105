7a491e597cf2f694ec5455560e4667c46cb5f3f5
Zac <a19860101@gmail.com>
2024-12-05 19:59:04 +0800

    20241205

--- web/public/index.php
+++ web/public/index.php
@@ -11,10 +11,10 @@
 
 ?>
 <?php include('template/header.php'); ?>
-<div class="container mx-auto">
+<div class="container mx-auto space-y-8">
 
 <?php foreach($posts as $post){ ?>
-    <div>
+    <div class="bg-zinc-200 p-4 rounded-lg">
         <h2 class="text-xl font-bold"><?php echo $post['title']; ?></h2>
         <div><?php echo $post['created_at'];?></div>
         <div>
